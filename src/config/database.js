aeb42369690b88e45b60c51d8e5602863d9ef360
vivekprakash2528 <vivekprakash2528@gmail.com>
2026-03-08 17:15:38 +0530

    config

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,7 @@
 import postgres from "postgres";
 import { _config } from "./config.js";
 import { drizzle } from "drizzle-orm/postgres-js";
+//db configuration
 
 export const connection = postgres(_config.DATABASE_URI);
 export const db = drizzle(connection); 
